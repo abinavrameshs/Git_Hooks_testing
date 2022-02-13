12356d9f9c7bac33d5a58e72f5eae54e0bb1610a
Abinav Ramesh Sundararaman <abinavrameshs@gmail.com>
2022-02-13 09:29:03 -0500

    Change .bat file

--- install_hooks.bat
+++ install_hooks.bat
@@ -11,7 +11,13 @@
 ::set LIB_DIR=submodules
 
 if exist .git\hooks\commit-msg del /F .git\hooks\commit-msg
-mklink .git\hooks\commit-msg ..\..\src\commit-msg.py
+mklink .git\hooks\commit-msg ..\..\hooks\commit-msg.py
+
+if exist .git\hooks\pre-commit del /F .git\hooks\pre-commit
+mklink .git\hooks\pre-commit ..\..\hooks\pre-commit.py
+
+if exist .git\hooks\pre-rebase del /F .git\hooks\pre-rebase
+mklink .git\hooks\pre-rebase ..\..\hooks\pre-rebase.py
 
 ::if exist .git\hooks\%LIB_DIR% rmdir /S /Q .git\hooks\%LIB_DIR%
 ::mklink /D .git\hooks\%LIB_DIR% ..\..\src\%LIB_DIR%
